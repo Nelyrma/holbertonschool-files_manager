0f1a977a34e4a19e4735f7c346be93dfcdec0ef5
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-04-11 10:19:48 +0200

    Fix syntax errors

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,19 @@
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-export default class AppController {
-  static getStatus(req, res) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    res.status(200).send(status);
+class AppController {
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
-  static async getStatus(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    res.status(200).send(stats);
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    res.status(200).json({ users, files });
   }
 }
+export default AppController;
